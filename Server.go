55e5f7199aeae99312e81a412f4a18e0ea1bf8a4
dxyinme <dxyinme@outlook.com>
2020-05-20 08:48:56 +0800

    rebuild

--- Server.go
+++ Server.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"Luka/Keeper"
-	pb "Luka/proto"
 	"Luka/util"
-	"google.golang.org/grpc"
 	"log"
-	"net"
 )
 
 
@@ -17,15 +13,4 @@ func main(){
 	}
 	log.Println(conf)
 	log.Println("hello Register!!!")
-	sev , errTCP := net.Listen("tcp" , conf.RegisterPort)
-	if errTCP != nil {
-		log.Println(errTCP)
-	}
-	s := grpc.NewServer()
-	pb.RegisterKeeperServer(s, &Keeper.Server{})
-	// 心跳机制
-	go Keeper.CircleConfirm()
-	if err := s.Serve(sev); err != nil {
-		log.Println(err)
-	}
 }
