99ad31845c8a91b666147822fd8d627607c68008
dxyinme <dxyinme@outlook.com>
2020-12-27 17:06:05 +0800

    support for udpsendmsg

--- main/KeeperDeployment.go
+++ main/KeeperDeployment.go
@@ -4,17 +4,40 @@ import (
 	"flag"
 	"github.com/dxyinme/Luka/WorkerPool"
 	ClusterConfig "github.com/dxyinme/Luka/cluster/config"
+	"github.com/dxyinme/LukaComm/CynicU/SendMsg"
 	CynicUServer "github.com/dxyinme/LukaComm/CynicU/Server"
 	"github.com/golang/glog"
 	"os"
 )
 
 var (
-	// --IFC : is the config is in file
+	// -IFC : is the config is in file
 	isFileConfig = flag.Bool("IFC", false, "is file config")
-	// --ICC : is commandline config
+	// -ICC : is commandline config
 	isCmdConfig = flag.Bool("ICC", false, "is commandline config")
+
+	udpServerAddr = flag.String("udpAddr", ":12999", "udp sendMsg.Server listener")
 )
+
+func setUpUDPServer(w *WorkerPool.NormalImpl) {
+	s := SendMsg.NewServer()
+
+	go func() {
+		if err := s.Listen(*udpServerAddr); err != nil {
+			glog.Fatal(err)
+		}
+
+	}()
+
+	go func() {
+		for {
+			msgNow := s.GetMsg()
+			w.SendTo(msgNow)
+		}
+	}()
+
+	glog.Info("udp server set up finished")
+}
 
 func main() {
 	flag.Parse()
@@ -39,6 +62,9 @@ func main() {
 	normalImpl.Initial()
 	defer normalImpl.Reduce()
 	s.BindWorkerPool(normalImpl)
+	// set up udp server
+	setUpUDPServer(normalImpl)
+
 	if err := server.Serve(s.Lis); err != nil {
 		glog.Fatalf("Server failed because of %v", err)
 	}
