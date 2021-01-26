1a78460464dd6e71076278a41d11c7759ffaed28
dxyinme <dxyinme@outlook.com>
2021-01-26 20:41:34 +0800

    update dbserver1

--- Dao/GroupDao/MainInfoInGroupDao.go
+++ Dao/GroupDao/MainInfoInGroupDao.go
@@ -5,5 +5,4 @@ type MainInfoInGroupDao interface {
 	DeleteGroup(groupName string, uid string) error
 	// get all group and master-user of them.
 	GetAllGroup() ([]string, []string, error)
-	//GroupGetAllUser(groupName string) ([]string, error)
 }
